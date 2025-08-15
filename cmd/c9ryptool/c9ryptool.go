b2c88c8437d8ba7476b564c53e1e991caf92e1f7
pangduckwai <paul.lai@sea9.org>
2025-08-15 19:09:48 +0800

    Add encoding (WIP)

--- cmd/c9ryptool/c9ryptool.go
+++ cmd/c9ryptool/c9ryptool.go
@@ -5,10 +5,27 @@ import (
 	"log"
 	"os"
 
-	"sea9.org/go/cryptool/pkg/algs"
 	"sea9.org/go/cryptool/pkg/cfgs"
 	"sea9.org/go/cryptool/pkg/cryptool"
+	algs "sea9.org/go/cryptool/pkg/encrypt"
 )
+
+func listCy() {
+	fmt.Println(cfgs.Desc())
+	for i, n := range algs.List() {
+		a := algs.Get(n)
+		if a.Type() {
+			fmt.Printf(" %2v sym  %v\n", i+1, n)
+		} else {
+			fmt.Printf(" %2v asym %v\n", i+1, n)
+		}
+	}
+}
+
+func listCd() {
+	fmt.Println(cfgs.Desc())
+	fmt.Println("TEMP!!! HAHAHA list encoding schemes!")
+}
 
 func main() {
 	cfg, err := cfgs.Parse(os.Args)
@@ -16,7 +33,7 @@ func main() {
 		log.Fatalf("[MAIN]%v\n%v\n%v\n", err, cfgs.Desc(), cfgs.Usage())
 	}
 
-	switch cfg.Command {
+	switch cfg.Command() {
 	case 0:
 		fallthrough
 	case 1:
@@ -25,37 +42,45 @@ func main() {
 			log.Fatalf("[MAIN]%v", err)
 		}
 
+		if cfg.IsList() {
+			listCy()
+			return
+		}
+
 		algr := algs.Get(algs.Parse(cfg.Algr))
 		if algr == nil {
 			log.Fatalf("[MAIN] unsupported algorithm '%v'", cfg.Algr)
 		}
 
-		switch cfg.Command {
-		case 0:
+		if cfg.Command() == 0 {
 			err = cryptool.Encrypt(cfg, algr)
-		case 1:
+		} else {
 			err = cryptool.Decrypt(cfg, algr)
 		}
 
 		if cfg.Verbose {
 			fmt.Printf("%v finished using '%v'\n", cfgs.Desc(), algr.Name())
 		}
-	case 7:
-		fmt.Println(cfgs.Desc())
-		for i, n := range algs.List() {
-			a := algs.Get(n)
-			if a.Type() {
-				fmt.Printf(" %2v sym  %v\n", i+1, n)
-			} else {
-				fmt.Printf(" %2v asym %v\n", i+1, n)
-			}
+
+	case 2:
+		fallthrough
+	case 3:
+		err = cfgs.Validate(cfg)
+		if err != nil {
+			log.Fatalf("[MAIN]%v", err)
 		}
+
+		if cfg.IsList() {
+			listCd()
+			return
+		}
+
 	case 8:
 		fmt.Printf("%v\n%v\n", cfgs.Desc(), cfgs.Help())
 	case 9:
 		fmt.Println(cfgs.Desc())
 	default:
-		err = fmt.Errorf(" unknown command '%v'", cfg.Command)
+		err = fmt.Errorf(" unknown command '%v'", cfg.Command())
 	}
 
 	if err != nil {
